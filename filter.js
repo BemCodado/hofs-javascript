b7938a87de3080ae62812653e61537859453ba45
cassimiroclaudio <claudio.cassimiro@imobles.com>
2022-08-31 15:17:42 -0300

    feat: implements logic to filter an array without hofs

---
+++ filter.js
@@ -0,0 +1,19 @@
+const array = [10, 15, 20, 25, 30];
+
+const filter = (array, comparador) => {
+  const resultado = [];
+
+  for (let index = 0; index < array.length; index++) {
+    if (array[index] > comparador) {
+      resultado.push(array[index]);
+    }
+  }
+
+  return resultado;
+};
+
+const maioresQueVinte = filter(array, 20);
+
+console.log(maioresQueVinte); // [ 25, 30 ]
+
+// ========================================================= //
