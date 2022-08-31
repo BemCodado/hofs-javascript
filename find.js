27d24f23812a84d559caca09fc8668da915bf08d
cassimiroclaudio <claudio.cassimiro@imobles.com>
2022-08-31 15:17:54 -0300

    feat: implements logic to find an element in an array without hofs

---
+++ find.js
@@ -0,0 +1,15 @@
+const array = [10, 15, 20, 25, 30];
+
+const find = (array, comparador) => {
+  for (let index = 0; index < array.length; index++) {
+    if (array[index] > comparador) {
+      return array[index];
+    }
+  }
+};
+
+const maiorQueVinte = find(array, 20);
+
+console.log(maiorQueVinte); // 25
+
+// ========================================================= //
