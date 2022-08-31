06d60185524fea8bdc27df0bee5bc4fcdaf889ac
cassimiroclaudio <claudio.cassimiro@imobles.com>
2022-08-31 15:18:17 -0300

    feat: implements logic to running an array element by element without hofs

---
+++ forEach.js
@@ -0,0 +1,17 @@
+const array = [10, 15, 20, 25, 30];
+
+const forEach = (array) => {
+  for (let index = 0; index < array.length; index++) {
+    console.log('array[ ' + index + ' ] = ' + array[index]);
+  }
+};
+
+forEach(array);
+
+// array[ 0 ] = 10
+// array[ 1 ] = 15
+// array[ 2 ] = 20
+// array[ 3 ] = 25
+// array[ 4 ] = 30
+
+// ========================================================= //
