36a9beb93e27084c8c3088bd5511d6564598a933
cassimiroclaudio <claudio.cassimiro@imobles.com>
2022-09-01 20:41:30 -0300

    feat: implements the same logic with  hof filter

--- filter.js
+++ filter.js
@@ -1,19 +1,25 @@
-const array = [10, 15, 20, 25, 30];
+// const array = [10, 15, 20, 25, 30];
 
-const filter = (array, comparador) => {
-  const resultado = [];
+// const filter = (array, comparador) => {
+//   const resultado = [];
 
-  for (let index = 0; index < array.length; index++) {
-    if (array[index] > comparador) {
-      resultado.push(array[index]);
-    }
-  }
+//   for (let index = 0; index < array.length; index++) {
+//     if (array[index] > comparador) {
+//       resultado.push(array[index]);
+//     }
+//   }
 
-  return resultado;
-};
+//   return resultado;
+// };
 
-const maioresQueVinte = filter(array, 20);
+// const maioresQueVinte = filter(array, 20);
 
-console.log(maioresQueVinte); // [ 25, 30 ]
+// console.log(maioresQueVinte); // [ 25, 30 ]
 
 // ========================================================= //
+
+const array = [10, 15, 20, 25, 30];
+
+const result = array.filter((element) => element > 20);
+
+console.log(result);
