90df24157a7c574b6494a5ec1473c683dc0a7ddd
cassimiroclaudio <claudio.cassimiro@imobles.com>
2022-09-01 20:41:39 -0300

    feat: implements the same logic with  hof find

--- find.js
+++ find.js
@@ -1,15 +1,21 @@
-const array = [10, 15, 20, 25, 30];
+// const array = [10, 15, 20, 25, 30];
 
-const find = (array, comparador) => {
-  for (let index = 0; index < array.length; index++) {
-    if (array[index] > comparador) {
-      return array[index];
-    }
-  }
-};
+// const find = (array, comparador) => {
+//   for (let index = 0; index < array.length; index++) {
+//     if (array[index] > comparador) {
+//       return array[index];
+//     }
+//   }
+// };
 
-const maiorQueVinte = find(array, 20);
+// const maiorQueVinte = find(array, 20);
 
-console.log(maiorQueVinte); // 25
+// console.log(maiorQueVinte); // 25
 
 // ========================================================= //
+
+const array = [10, 15, 20, 25, 30];
+
+const result = array.find((element) => element > 20);
+
+console.log(result);
